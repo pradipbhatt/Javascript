20543111a6a6d60683d44e544c720cacb7e15065
PRADIP BHATT <bhattsammar04@gmail.com>
2023-03-16 23:13:58 +0545

    init commit

---
+++ data_types.js
@@ -0,0 +1,15 @@
+//nn bb ss u
+let a=null;
+let b=45;
+let c=true;
+let d=BigInt(123);
+let e="Pradip"
+let f=Symbol("I am pradip Bhatt CSE student");
+let g;     //undefined while we don't decleare the data type.
+console.log(a,b,c,d,e,f,g)   
+// In above line we simply print the data types
+console.log(typeof c);
+// Here we print the type of c
+console.log(typeof f);
+// Here we print the type of f
+
---
+++ data_types2.js
@@ -0,0 +1,52 @@
+// Numeric data type
+var num = 123;
+
+// String data type
+var str = "Hello, world!";
+
+// Boolean data type
+var bool = true;
+
+// Undefined data type
+var und = undefined;
+
+// Null data type
+var nul = null;
+
+// Object data type
+var obj = {name: "John", age: 30};
+
+// Array data type
+var arr = [1, 2, 3];
+
+// Function data type
+var func = function() {
+  console.log("This is a function.");
+};
+
+// Symbol data type (added in ES6)
+var sym = Symbol("This is a symbol");
+
+console.log(typeof num); // "number"
+console.log(typeof str); // "string"
+console.log(typeof bool); // "boolean"
+console.log(typeof und); // "undefined"
+console.log(typeof nul); // "object"
+console.log(typeof obj); // "object"
+console.log(typeof arr); // "object"
+console.log(typeof func); // "function"
+console.log(typeof sym); // "symbol"
+
+
+
+/*JavaScript has 9 data types in total:
+
+Number
+String
+Boolean
+Undefined
+Null
+Object
+Array
+Function
+Symbol (added in ES6)*/
