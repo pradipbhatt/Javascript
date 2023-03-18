be0da223dc237a085f31cd011cdfd5cf81446886
PRADIP BHATT <bhattsammar04@gmail.com>
2023-03-18 08:26:50 +0545

    day 2 changes for var let const

---
+++ let_var_const.js
@@ -0,0 +1,42 @@
+console.log("Javascript Tutorial 3 by pradip bhatt")
+// let a=40;
+// var b="Pradip";
+// var c=null;
+// var d=undefined;
+// var is globally decleared so if once we decleared it can again redecleared and last ma j declearfed cha tae value print hunca top decleared print i.e executed hunna so we use let variable
+
+
+
+let a=40;
+let b="Pradip";
+let c=null;
+let d=undefined;
+console.log(a,b,c,d)
+
+// var can be updated and redecleared within it's scope but let ko case mah redecleared hunna
+// let b="bhatt";
+// console.log(b);
+{
+  
+let b="I am redecleared";
+  console.log(b);
+  // let ko scope block level mah hunca so yo block vitra ko let ko scope bahira hunna
+}
+// if hami le let ko use garexi first block level execute hunca then outer wala execute hunca 
+
+// Const can neither be updated nor be redecleared
+// Const hami tab use garxou when we know the value of some variable remains constant always
+
+const host="pradip";
+// let host=5; if hami ley host variable lae feri use garna khojey it is already redecleared vanxa so 
+
+console.log(host);
+
+
+
+
+// summary var rarely use garnu
+// always const and let use garnu parne+
+
+
+
