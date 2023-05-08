ab92cf5e8c0aa729036699568515820bdc3445d3
PRADIP BHATT <bhattsammar04@gmail.com>
2023-05-08 22:45:34 +0545

    JS_learning_through_WScubetech-day-1

---
+++ first_day/function.js
@@ -0,0 +1,16 @@
+alert("Hello guys, \n how's the day going? \n Actually i am here from alert function .");
+
+// second function
+confirm("Do you want to login in premiun account? \n Actually i am here from confirm funcftion");
+// shows confirm box
+
+console.log("Hey buddy you're developer now \n coz you can see this message in console");
+
+document.write(" <h1> Hello i am here from document . write function</h1>")
+
+// promt function
+// shows promt box it allows user to enter some value
+// howto store value from promt box
+var name=prompt("Enter your name :");
+let caste=prompt("Enter your caste :");
+document.write(" <h1> hello "+"\n I am from prompt function \n " + "\n"+ name +caste+"\n"+ "</h1>");
---
+++ second-day/script_variable.js
@@ -0,0 +1,6 @@
+// we practise about variables
+let a=40;
+let b="pradip";
+let name="pradip";
+alert(a+b);
+alert(name + "\t  "+ "is my name");
